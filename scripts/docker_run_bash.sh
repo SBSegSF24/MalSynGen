a6656365399a70a764b86d8ba12de72317755580
A-G-D24 <angelonogueira.aluno@unipampa.edu.br>
2024-07-06 23:03:49 -0300

    Update docker_run_bash.sh

--- scripts/docker_run_bash.sh
+++ scripts/docker_run_bash.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run -it --name=syntabdata-$RANDOM -e DISPLAY=unix$DISPLAY sf24/syntabdata:latest bash
+sudo docker run -it --name=MalSynGen-$RANDOM -e DISPLAY=unix$DISPLAY sf24/malsyngen:latest bash
