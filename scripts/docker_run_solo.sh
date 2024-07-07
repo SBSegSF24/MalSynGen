03782a86d65de144cce6db9d4b1d1424aa7dc437
A-G-D24 <angelonogueira.aluno@unipampa.edu.br>
2024-07-06 23:04:40 -0300

    Update docker_run_solo.sh

--- scripts/docker_run_solo.sh
+++ scripts/docker_run_solo.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run -it --name=syn_tab_data-$RANDOM -e DISPLAY=unix$DISPLAY sf24/syn_tab_data:latest
+sudo docker run -it --name=MalSynGen-$RANDOM -e DISPLAY=unix$DISPLAY sf24/malsyngen:latest
