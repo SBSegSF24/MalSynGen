b157410b4691bddff2fdb072f0bcc3d4377737a2
A-G-D24 <angelonogueira.aluno@unipampa.edu.br>
2024-07-08 10:35:39 -0300

    Add files via upload

--- scripts/docker_run_solo.sh
+++ scripts/docker_run_solo.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+if [ -z "$(sudo docker images -q sf24/malsyngen:latest 2> /dev/null)" ]; then
+ ./scripts/docker_build.sh
+fi
+
 sudo docker run -it --name=MalSynGen-$RANDOM -e DISPLAY=unix$DISPLAY sf24/malsyngen:latest
