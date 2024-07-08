1cbe7b518809b4a10d929b008448ece8ef92bb91
A-G-D24 <angelonogueira.aluno@unipampa.edu.br>
2024-07-08 18:28:03 -0300

    demo reduzido

--- run_demo_app.sh
+++ run_demo_app.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-pipenv run python3 main.py -i datasets/kronodroid_real_device-balanced.csv  --num_samples_class_benign 10000 --num_samples_class_malware 10000 --batch_size 256 --number_epochs 300 --k_fold 10
+pipenv run python3 main.py -i datasets/kronodroid_real_device-balanced.csv  --num_samples_class_benign 10000 --num_samples_class_malware 10000 --batch_size 256 --number_epochs 100 --k_fold 2
+
