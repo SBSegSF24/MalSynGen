849d4b0a0c66822874cb743f7ad2f4d81bdcac21
A-G-D24 <angelonogueira.aluno@unipampa.edu.br>
2024-07-10 11:24:03 -0300

    Update docker_run_bash.sh

--- scripts/docker_run_bash.sh
+++ scripts/docker_run_bash.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-sudo docker run -it --name=MalSynGen-$RANDOM -e DISPLAY=unix$DISPLAY sf24/malsyngen:latest bash
+if docker info >/dev/null 2>&1; then
+  docker run -it --name=MalSynGen-$RANDOM -e DISPLAY=unix$DISPLAY sf24/malsyngen:latest bash
+else
+    echo "Seu usuário atual não possui permissões para executar docker sem sudo, execute o seguinte comando e reinicialize a máquina: sudo usermod -aG docker SEU_USER "
+fi
