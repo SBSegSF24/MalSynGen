78f3ce1033409ff1df62ba6378422f32964999b2
A-G-D24 <angelonogueira.aluno@unipampa.edu.br>
2024-07-10 11:23:31 -0300

    Update docker_rm_all.sh

--- scripts/docker_rm_all.sh
+++ scripts/docker_rm_all.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+if docker info >/dev/null 2>&1; then
+	for i in $( docker ps -a | awk '{print $1}' | grep -v CONTAINER)
+	do 
+		 docker rm -f $i
+	done
+else
+    echo "Seu usuário atual não possui permissões para executar docker sem sudo, execute o seguinte comando e reinicialize a máquina: sudo usermod -aG docker SEU_USER "
+fi
 
-for i in $(sudo docker ps -a | awk '{print $1}' | grep -v CONTAINER)
-do 
-	sudo docker rm -f $i
-done
