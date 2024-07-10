02ba5140ec8dbe0501f8dba823afe3ad646c2d0f
A-G-D24 <angelonogueira.aluno@unipampa.edu.br>
2024-07-09 22:38:52 -0300

    Add files via upload

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-docker build -t sf24/malsyngen:latest . 
+if docker info >/dev/null 2>&1; then
+	docker build -t sf24/malsyngen:latest . 
+else
+    echo "Seu usuário atual não possui permissões para executar docker sem sudo, execute o seguinte comando e reinicialize a máquina: sudo usermod -aG docker SEU_USER "
+fi
