ad95b4f75d7bd7813f3740701d96b401e7674f2d
A-G-D24 <angelonogueira.aluno@unipampa.edu.br>
2024-07-09 22:45:44 -0300

    Add files via upload

--- run_demo_docker.sh
+++ run_demo_docker.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #USER_ID=$(id -u $USER)
-DIR=$(readlink -f shared)
-docker build -t sf24/malsyngen:latest . 
-docker run -it --name=MalSynGen-$RANDOM -v $DIR:/SynTabData/shared -e DISPLAY=unix$DISPLAY sf24/malsyngen:latest /MalSynGen/shared/app_run.sh --verbosity 20 --output_dir /MalSynGen/shared/outputs --input_dataset /MalSynGen/datasets/kronodroid_emulador-balanced.csv --num_samples_class_malware  10000 --num_samples_class_benign 10000 --batch_size 256   --number_epochs 300 --k_fold 10 --training_algorithm Adam
-#ls shared/outputs/
+if docker info >/dev/null 2>&1; then
+	DIR=$(readlink -f shared)
+	docker build -t sf24/malsyngen:latest . 
+	docker run -it --name=MalSynGen-$RANDOM -v $DIR:/SynTabData/shared -e DISPLAY=unix$DISPLAY sf24/malsyngen:latest /MalSynGen/shared/app_run.sh --verbosity 20 --output_dir /MalSynGen/shared/outputs --input_dataset /MalSynGen/datasets/kronodroid_real_device-balanced.csv  --num_samples_class_malware  10000 --num_samples_class_benign 10000 --batch_size 256   --number_epochs 100 --k_fold 2 --training_algorithm Adam
+	#ls shared/outputs/
+else
+    echo "You need sudo to run Docker."
+fi
