5a7a8dc012152810368618cf5593a9c8946ed4ec
A-G-D24 <angelonogueira.aluno@unipampa.edu.br>
2024-07-09 23:50:53 -0300

    Add files via upload

--- run_balanced_datasets.sh
+++ run_balanced_datasets.sh
@@ -1,31 +1,59 @@
-#!/bin/sh
-read -p "input batch_size :" batch
-read -p "input for number of epcochs :" epochs
-read -p "input the values of layers :" layers
-read -p "number of folds :" folds
-names=$(ls datasets/validation/| grep validation.txt | cut -d  "-" -f 1) 
-list=()
-for nam in $names;do 
-      s=datasets/validation/$nam"-validation.txt"
-      r=datasets/$nam"-balanced.csv"
-      if [[ $layers == *","* ]]; then
-             lay1=$(echo $layers|cut -d "," -f 1)
-             lay2=$(echo $layers|cut -d "," -f 2)
-             a=$(head -n 1 $s| cut -d " " -f 5)
-             pipenv run python3 main.py  -i $r -o $nam --num_samples_class_benign $a  --num_samples_class_malware $a --batch_size $batch  --number_epochs $epochs  --dense_layer_sizes_g $lay1,$lay2 --dense_layer_sizes_d $lay2,$lay1 --k_fold $folds 
-      else
-          a=$(head -n 1 $s| cut -d " " -f 5)
-          pipenv run python3 main.py  -i $r -o $nam --num_samples_class_benign $a  --num_samples_class_malware $a --batch_size $batch  --number_epochs $epochs  --dense_layer_sizes_g $layers --dense_layer_sizes_d $layers  --k_fold $folds
-      fi
-      echo "executado"
-      #list+=($a)
-      
-      
+#!/bin/bash
+
+# Default values
+batch=256
+epochs=100
+layers=512
+folds=2
+
+# User inputs
+read -p "Input batch size: " batch
+read -p "Input number of epochs: " epochs
+read -p "Input the values of layers: " layers
+read -p "Number of folds: " folds
+if [[ -z "$batch" ]]; then
+batch=256
+fi
+if [[ -z "$epochs" ]]; then
+epochs=100
+fi
+if [[ -z "$layers" ]]; then
+layers=512
+fi
+if [[ -z "$folds" ]];then
+folds=2
+fi
+# Get dataset names
+names=$(ls datasets/validation/ | grep validation.txt | cut -d "-" -f 1)
+
+# Process each dataset
+for name in $names; do
+    s="datasets/validation/${name}-validation.txt"
+    r="datasets/${name}-balanced.csv"
+    num_samples=$(head -n 1 "$s" | cut -d " " -f 5)
+
+    if [[ "$layers" == *","* ]]; then
+        lay1=$(echo $layers | cut -d "," -f 1)
+        lay2=$(echo $layers | cut -d "," -f 2)
+        pipenv run python3 main.py -i "$r" -o "$name" \
+            --num_samples_class_benign "$num_samples" \
+            --num_samples_class_malware "$num_samples" \
+            --batch_size "$batch" \
+            --number_epochs "$epochs" \
+            --dense_layer_sizes_g "$lay1,$lay2" \
+            --dense_layer_sizes_d "$lay2,$lay1" \
+            --k_fold "$folds"
+    else
+        pipenv run python3 main.py -i "$r" -o "$name" \
+            --num_samples_class_benign "$num_samples" \
+            --num_samples_class_malware "$num_samples" \
+            --batch_size "$batch" \
+            --number_epochs "$epochs" \
+            --dense_layer_sizes_g "$layers" \
+            --dense_layer_sizes_d "$layers" \
+            --k_fold "$folds"
+    fi
+
+    echo "Executed for $name"
 done
-#for i in ${list[*]};do 
- #	pipenv run python3 main_aim_mlflow.py -ml true -i r
-#done
-echo ${list[*]}
-
-
 
