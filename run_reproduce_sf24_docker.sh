64b9c49c918c668a1b4f35ad6fe984b2f04c8ba5
A-G-D24 <angelonogueira.aluno@unipampa.edu.br>
2024-07-31 15:29:50 -0300

    Update run_reproduce_sf24_docker.sh

--- run_reproduce_sf24_docker.sh
+++ run_reproduce_sf24_docker.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #USER_ID=$(id -u $USER)
 if docker info >/dev/null 2>&1; then
-	DIR=$(readlink -f shared)
-	if [ -z "$(docker images -q sf24/malsyngen:latest 2> /dev/null)" ]; then
-	docker build -t sf24/malsyngen:latest . 
-	fi
-	docker run -it --name=MalSynGen-$RANDOM -v $DIR:/SynTabData/shared -e DISPLAY=unix$DISPLAY sf24/malsyngen:latest /MalSynGen/run_reproduce_sf24.sh
-	#ls shared/outputs/
+        DIR=$(readlink -f campanhas_SF24)
+        if [ -z "$(docker images -q sf24/malsyngen:latest 2> /dev/null)" ]; then
+        docker build -t sf24/malsyngen:latest . 
+        fi
+        echo $DIR
+        docker run -it --name=MalSynGen-$RANDOM -v $DIR:/MalSynGen/campanhas_SF24 -e DISPLAY=unix$DISPLAY sf24/malsyngen:latest /MalSynGen/run_reproduce_sf24_venv.sh
+        #ls shared/outputs/
 else
     echo "Seu usuário atual não possui permissões para executar docker sem sudo, execute o seguinte comando e reinicialize a máquina: sudo usermod -aG docker SEU_USER "
 fi
@@ -17,3 +18,10 @@ else
    pip install notebook
    jupyter notebook plots.ipynb
 fi
+
+
+
+
+
+
+
